d9b32150f1a31a3eaa0dc4e8fbea558508c567e3
Ashok Mehta <52393723+ashok2024@users.noreply.github.com>
2022-01-26 19:19:49 +0530

    base version

---
+++ Startup.cs
@@ -0,0 +1,43 @@
+﻿namespace VMS_API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get;}
+        public Startup(IConfiguration configuration)
+        { 
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("ServiceAPIPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer(); 
+            services.AddSwaggerGen();
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseCors("ServiceAPIPolicy");
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapControllers();
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+        }
+    }
+}
